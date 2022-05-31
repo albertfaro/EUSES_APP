1438eeba811b2027bd9dfeea2cab2bf15e5c55ee
Albert Faro <73467798+albertfaro@users.noreply.github.com>
2022-05-31 14:49:41 +0200

    Various HUD changes and 2D World selector

---
+++ Assets/Scripts/NewWorldSelector.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewWorldSelector : MonoBehaviour
+{
+    public List<GameObject> planets;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void nextPlanet()
+    {
+
+    }
+
+
+    public void previousPlanet()
+    {
+
+    }
+
+    public void getToPlanet(string planetName)
+    {
+        SceneManager.LoadScene(planetName);
+    }
+}
